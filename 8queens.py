0275da9a1cdbf30279849d3add3c4187eb4562dc
Viktória Nagy <viki.nagy.98@gmail.com>
2021-04-27 00:22:41 +0200

    n×k, where n <= k and n and k != 2 or 3

--- 8queens.py
+++ 8queens.py
@@ -1,8 +1,9 @@
 #import numpy as np
 import math
 
-def show(state):
-    baseStr = list("........")
+def show(state, n, k):
+
+    baseStr = ['.'] * k
     for row in state:
         str = baseStr.copy()
         if row > -1:
@@ -25,36 +26,63 @@ def isOK(r, c, state):
             return False
     return True
 
+
+def giveParams():
+    print("Give the number of the rows: ")
+    n = int(input())
+    print("Give the number of the columns: ")
+    k = int(input())
+    print("The size of the board:", n, " × ", k)
+    return n, k
+
+
+def numOfQueens(n, k):
+    if n >= k:
+        q = k
+    else:
+        q = n
+    return q
+
+
 # Find solutions
+
 solutions = []
 
-queens = [-1, -1, -1, -1, -1, -1, -1, -1]
+n, k = giveParams()
+
+q = numOfQueens(n, k)
+queens = [-1]*q
+
+print(queens)
 r = 0
-while r < 8 and r > -1:
-    r_ok = False
-    for c in range(queens[r] + 1, 8):
-        if isOK(r, c, queens):
-            queens[r] = c
-            r_ok = True
-            break
+c = 0
 
-    if not r_ok:
-        queens[r] = -1
-        r = r - 1
-    else:
-        r = r + 1
+if k >= n:
+    while r < k and r > -1:
+        r_ok = False
+        for c in range(queens[r] + 1, k):
+            if isOK(r, c, queens):
+                queens[r] = c
+                r_ok = True
+                break
 
-    if r == 8:
-        solutions.append(queens.copy())
-        queens[r - 1] = -1
-        r = r - 2
+        if not r_ok:
+            queens[r] = -1
+            r = r - 1
+        else:
+            r = r + 1
+
+        if r == n:
+                solutions.append(queens.copy())
+                r = r - 1
 
 # Test solutions
 errors = []
+
 # Test1 - testing every raw in all solutions
 for i, solution in enumerate(solutions):
     for r, c in enumerate(solutions[i]):
-        if c < 0 or c > 7:
+        if c < 0 or c > k - 1:
             errors.append((1, i, r))
 
 # Test2 - testing every queen in all solutions
@@ -74,7 +102,7 @@ if len(errors) == 0:
     num_of_solutions = len(solutions)
     for i, solution in enumerate(solutions):
         print("{}/{}".format((i + 1), num_of_solutions))
-        show(solution)
+        show(solution, n, k)
 else:
     print("Something went wrong!")
     print("(Test case, solution, row of solution)")
